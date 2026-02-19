e9fed9448a17fc2e232a456f13b7b5140bc84cf4
khushangpatel <khushangpatel22@gnu.ac.in>
2026-02-19 11:30:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Customer Predictor", layout="wide")
+
+st.title("Customer Prediction App")
+st.write("Use the sidebar to navigate between pages.")
